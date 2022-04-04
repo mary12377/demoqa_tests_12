702a80d23e82d37c06421d78fdb4c73676e028b9
Mikhail <bulanjova@mail.ru>
2022-04-04 13:46:53 +0300

    add

--- src/test/java/tests/Junit5Tests.java
+++ src/test/java/tests/Junit5Tests.java
@@ -1,4 +1,4 @@
-package guru.qa;
+package tests;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,66 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TextBoxTests {
+
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTests() {
+        String firstName = "Maria";
+        String lastName = "Bulanjova";
+        String email = "maria@mail.ru";
+        String Mobil = "89817572792";
+        String Subjects = "History";
+        String Hobbies = "Music";
+        String imgPath = "image.JPG";
+        String address = "academician yangel street";
+
+        Selenide.open("/automation-practice-form");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText("Female")).click();
+        $("#userNumber").setValue(Mobil);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(byText("6")).click();
+        $("#subjectsInput").setValue(Subjects).pressEnter();
+        $("#hobbiesWrapper").$(byText(Hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath(imgPath);
+        $("#currentAddress").setValue(address);
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive") .shouldHave(text("firstName lastName"),
+
+                text(email),
+                text(Mobil),
+                text(Subjects),
+                text(Hobbies),
+                text(imgPath),
+                text(address));
+
+    }
+    }
+
